2c3f311ee405a681bd8bc2ab2ebc21f63c5fbc6c
danilo <danilo@danilo-pc>
2014-03-15 12:03:04 -0300

    teste

--- TesteCas/src/main/java/local/teste/dao/EmployeeDAO.java
+++ TesteCas/src/main/java/local/teste/dao/EmployeeDAO.java
@@ -14,36 +14,45 @@ import local.teste.util.ObjectSizeFetcher;
 
 public class EmployeeDAO {
 	protected EntityManager entityManager;
-	
+
 	public EmployeeDAO() {
-        entityManager = getEntityManager();
-    }
-	
+		entityManager = getEntityManager();
+	}
+
 	private EntityManager getEntityManager() {
-        EntityManagerFactory factory = Persistence
-                .createEntityManagerFactory("EmployeePU");
-        if (entityManager == null) {
-            entityManager = factory.createEntityManager();
-        }
- 
-        return entityManager;
-    }
-	
-	@SuppressWarnings("unchecked")
-	public List<Employee> findAll() {
-		Query query = entityManager.createQuery("FROM " + Employee.class.getName());
-		query.setMaxResults(300000);
-		List<Employee> result = query.getResultList();
+		EntityManagerFactory factory = Persistence
+				.createEntityManagerFactory("EmployeePU");
 		try {
-			System.out.println(ObjectSizeFetcher.sizeOf(result)/1024/1024);
-		} catch (IllegalAccessException e) {
+			Thread.sleep(1200);
+		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		if (entityManager == null) {
+			entityManager = factory.createEntityManager();
+		}
+
+		return entityManager;
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Employee> findAll() {
+		long elapsedTime;
+		elapsedTime = System.currentTimeMillis();
+		Query query = entityManager.createQuery("FROM "
+				+ Employee.class.getName());
+		query.setMaxResults(300000);
+
+		List<Employee> result = query.getResultList();
+		elapsedTime = System.currentTimeMillis() - elapsedTime;
+		System.out.println("[MANUAL] Method " + this.getClass().getName()
+				+ " Executed in ms: " + elapsedTime);
+		/*
+		 * try { System.out.println(ObjectSizeFetcher.sizeOf(result)/1024/1024);
+		 * } catch (IllegalAccessException e) { // TODO Auto-generated catch
+		 * block e.printStackTrace(); }
+		 */
 		return result;
-    }
-	
-	
-	
-	
+	}
+
 }
