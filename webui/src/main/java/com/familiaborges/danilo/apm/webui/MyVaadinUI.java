6a75693f4e656d6901bbb0b5993226cf031bad30
danilobc <danilobc@imac-de-danilo.home>
2014-07-10 21:54:50 -0300

    Mais modificayoes

--- webui/src/main/java/com/familiaborges/danilo/apm/webui/MyVaadinUI.java
+++ webui/src/main/java/com/familiaborges/danilo/apm/webui/MyVaadinUI.java
@@ -1,40 +1,170 @@
 package com.familiaborges.danilo.apm.webui;
 
+import java.util.Locale;
+
 import javax.servlet.annotation.WebServlet;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.event.ShortcutAction.KeyCode;
+import com.vaadin.event.ShortcutListener;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
+import com.vaadin.shared.ui.label.ContentMode;
+import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Button.ClickListener;
+import com.vaadin.ui.CssLayout;
+import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.PasswordField;
+import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
 @Theme("mytheme")
-@SuppressWarnings("serial")
-public class MyVaadinUI extends UI
-{
+public class MyVaadinUI extends UI {
 
-    @WebServlet(value = "/*", asyncSupported = true)
-    @VaadinServletConfiguration(productionMode = false, ui = MyVaadinUI.class, widgetset = "com.familiaborges.danilo.apm.webui.AppWidgetSet")
-    public static class Servlet extends VaadinServlet {
-    }
+	private static final long serialVersionUID = 1L;
 
-    @Override
-    protected void init(VaadinRequest request) {
-        final VerticalLayout layout = new VerticalLayout();
-        layout.setMargin(true);
-        setContent(layout);
+	CssLayout root = new CssLayout();
+
+	private HelpManager helpManager;
+
+	CssLayout menu = new CssLayout();
+	CssLayout content = new CssLayout();
+	
+	VerticalLayout loginLayout;
+
+	@WebServlet(value = "/*", asyncSupported = true)
+	@VaadinServletConfiguration(productionMode = false, ui = MyVaadinUI.class, widgetset = "com.familiaborges.danilo.apm.webui.AppWidgetSet")
+	public static class Servlet extends VaadinServlet {
+	}
+
+	@Override
+	protected void init(VaadinRequest request) {
+
+		
+
+		helpManager = new HelpManager(this);
+
+		setLocale(Locale.US);
+		setContent(root);
+		
+		root.addStyleName("root");
+        root.setSizeFull();
         
-        Button button = new Button("Click Me");
-        button.addClickListener(new Button.ClickListener() {
+        Label bg = new Label();
+        bg.setSizeUndefined();
+        bg.addStyleName("login-bg");
+        root.addComponent(bg);
+
+		
+	}
+	
+	private void buildLoginView(boolean exit) {
+        if (exit) {
+            root.removeAllComponents();
+        }
+        helpManager.closeAll();
+        HelpOverlay w = helpManager
+                .addOverlay(
+                        "Welcome to the Dashboard Demo Application",
+                        "<p>This application is not real, it only demonstrates an application built with the <a href=\"http://vaadin.com\">Vaadin framework</a>.</p><p>No username or password is required, just click the ‘Sign In’ button to continue. You can try out a random username and password, though.</p>",
+                        "login");
+        w.center();
+        addWindow(w);
+
+        addStyleName("login");
+
+        loginLayout = new VerticalLayout();
+        loginLayout.setSizeFull();
+        loginLayout.addStyleName("login-layout");
+        root.addComponent(loginLayout);
+
+        final CssLayout loginPanel = new CssLayout();
+        loginPanel.addStyleName("login-panel");
+
+        HorizontalLayout labels = new HorizontalLayout();
+        labels.setWidth("100%");
+        labels.setMargin(true);
+        labels.addStyleName("labels");
+        loginPanel.addComponent(labels);
+
+        Label welcome = new Label("Welcome");
+        welcome.setSizeUndefined();
+        welcome.addStyleName("h4");
+        labels.addComponent(welcome);
+        labels.setComponentAlignment(welcome, Alignment.MIDDLE_LEFT);
+
+        Label title = new Label("QuickTickets Dashboard");
+        title.setSizeUndefined();
+        title.addStyleName("h2");
+        title.addStyleName("light");
+        labels.addComponent(title);
+        labels.setComponentAlignment(title, Alignment.MIDDLE_RIGHT);
+
+        HorizontalLayout fields = new HorizontalLayout();
+        fields.setSpacing(true);
+        fields.setMargin(true);
+        fields.addStyleName("fields");
+
+        final TextField username = new TextField("Username");
+        username.focus();
+        fields.addComponent(username);
+
+        final PasswordField password = new PasswordField("Password");
+        fields.addComponent(password);
+
+        final Button signin = new Button("Sign In");
+        signin.addStyleName("default");
+        fields.addComponent(signin);
+        fields.setComponentAlignment(signin, Alignment.BOTTOM_LEFT);
+
+        final ShortcutListener enter = new ShortcutListener("Sign In",
+                KeyCode.ENTER, null) {
+            @Override
+            public void handleAction(Object sender, Object target) {
+                signin.click();
+            }
+        };
+
+        signin.addClickListener(new ClickListener() {
+            @Override
             public void buttonClick(ClickEvent event) {
-                layout.addComponent(new Label("Thank you for clicking"));
+                if (username.getValue() != null
+                        && username.getValue().equals("")
+                        && password.getValue() != null
+                        && password.getValue().equals("")) {
+                    signin.removeShortcutListener(enter);
+                    //buildMainView();
+                } else {
+                    if (loginPanel.getComponentCount() > 2) {
+                        // Remove the previous error message
+                        loginPanel.removeComponent(loginPanel.getComponent(2));
+                    }
+                    // Add new error message
+                    Label error = new Label(
+                            "Wrong username or password. <span>Hint: try empty values</span>",
+                            ContentMode.HTML);
+                    error.addStyleName("error");
+                    error.setSizeUndefined();
+                    error.addStyleName("light");
+                    // Add animation
+                    error.addStyleName("v-animate-reveal");
+                    loginPanel.addComponent(error);
+                    username.focus();
+                }
             }
         });
-        layout.addComponent(button);
+
+        signin.addShortcutListener(enter);
+
+        loginPanel.addComponent(fields);
+
+        loginLayout.addComponent(loginPanel);
+        loginLayout.setComponentAlignment(loginPanel, Alignment.MIDDLE_CENTER);
     }
 
 }
--- webui/src/main/webapp/VAADIN/widgetsets/com.familiaborges.danilo.apm.webui.AppWidgetSet/deferredjs/DCAF61CF6BDEA83C4EFEC0998041B6C2/1.cache.js
+++
@@ -1,1 +0,0 @@
-$wnd.com_familiaborges_danilo_apm_webui_AppWidgetSet.runAsyncCallback1('Yab(1759,1,oDd);_.nc=function I2b(){YSb((!TSb&&(TSb=new ZSb),TSb),this.b.e)};Mqd(Zh)(1);\n//# sourceURL=com.familiaborges.danilo.apm.webui.AppWidgetSet-1.js\n')
